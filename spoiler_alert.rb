5639f1636616cc4c41fd6c570d7ab23ac0d57c3b
nodanaonlyzuul <stephen@eastmedia.com>
2012-07-04 12:28:42 -0400

    Initial commit

---
+++ spoiler_alert.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+require 'sinatra'
+require 'json'
+
+SPOILERS = YAML::load( File.open('./config/spoilers.yml') )
+
+get '/:topic' do
+  # require 'ruby-debug'; debugger
+  # params[:topic]
+  # content_type :json
+  {:spoiler => SPOILERS[params[:topic]] }.to_json
+end
