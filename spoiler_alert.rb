eb980c584e72b81f30b2f02c6f86021cde176323
nodanaonlyzuul <stephen@eastmedia.com>
2012-07-04 12:30:54 -0400

    adding an index action

--- spoiler_alert.rb
+++ spoiler_alert.rb
@@ -4,6 +4,10 @@ require 'json'
 
 SPOILERS = YAML::load( File.open('./config/spoilers.yml') )
 
+get '/' do
+  SPOILERS.keys.sort.to_json
+end
+
 get '/:topic' do
   # require 'ruby-debug'; debugger
   # params[:topic]
